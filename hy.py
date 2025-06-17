ae8e942cd81a1d1813e903a8031ffcbf918f0171
Tahseen-Frontend-Developer <tahseenbibi654@gmail.com>
2025-06-17 07:08:53 +0500

    Update hy.py

--- hy.py
+++ hy.py
@@ -1,7 +1,7 @@
-def division(a,b):
+def division(a,b,):
     return a/b
-def addition(a,b):
-    return(a+b)
+def addition(a,b,c):
+    return(a+b+c)
 def subtraction(a,b):
     return(a-b)
 def multipication(a,b):
