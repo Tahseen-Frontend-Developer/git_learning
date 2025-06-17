83a5ae69e811587357fdd68a40567ded05a4e4d8
Tahseen Bibi <tahseenbibi654@gmail.com>
2025-06-17 06:58:39 +0500

    Updated gitignore

--- hy.py
+++ hy.py
@@ -2,3 +2,9 @@ def division(a,b):
     return a/b
 def addition(a,b):
     return(a+b)
+def subtraction(a,b):
+    return(a-b)
+def multipication(a,b):
+    return(a*b)
+def division(x,y):
+    return x/y
