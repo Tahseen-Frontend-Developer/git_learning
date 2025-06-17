a29dc33b64355d31fd4b23c55e742895ff7367f6
Tahseen Bibi <tahseenbibi654@gmail.com>
2025-06-17 07:10:25 +0500

    subtraction updated code

--- hy.py
+++ hy.py
@@ -2,8 +2,8 @@ def division(a,b):
     return a/b
 def addition(a,b):
     return(a+b)
-def subtraction(a,b):
-    return(a-b)
+def subtraction(a,b,c):
+    return(a-b-c)
 def multipication(a,b):
     return(a*b)
 def addition(x,y):
